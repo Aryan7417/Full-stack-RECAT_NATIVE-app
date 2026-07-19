26bca6de68a5ce5557765d7a08913c87af4399a1
Aryan7417 <caryan830@gmail.com>
2026-07-19 16:00:52 +0530

    auth (Screen and tbes)

---
+++ client/src/app/(auth)/verify-emil.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const verify_emil = () => {
+  return (
+    <View>
+      <Text>verify-emil</Text>
+    </View>
+  )
+}
+
+export default verify_emil
